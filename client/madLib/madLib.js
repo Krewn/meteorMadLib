8adc6acf0dd80b3cc1203663e03675bd35f36839
Jon <conall.hawks@gmail.com>
2016-04-04 20:01:54 -0400

    Add functionality

--- client/madLib/madLib.js
+++ client/madLib/madLib.js
@@ -1,6 +1,10 @@
- Template.body.events({
-	 'submit form':function(event) {
-		 event.preventDefault();
-		 console.log("You Clicked Submit Nigga!");
-	 }
+Template.body.events({
+	'submit form': function(event) {
+		event.preventDefault();
+		var exc = event.target.exclamation.value;
+		var adv = event.target.adverb.value;
+		var ani = event.target.animal.value;
+		var vrb = event.target.verb.value;
+		console.log('\'' + exc + '!\' you say, as you ' + adv + ' jump onto your ' + ani + ' and ' + vrb + ' into the sunset.');
+	}
 });
